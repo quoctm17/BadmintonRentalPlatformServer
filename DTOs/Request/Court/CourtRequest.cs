b75db3e3340aec81649ceec6147b58c5109268f4
Quansucno <113964643+Quanntn0811@users.noreply.github.com>
2024-07-02 00:59:34 +0700

    create court

---
+++ DTOs/Request/Court/CourtRequest.cs
@@ -0,0 +1,20 @@
+﻿using BusinessObjects.Constants;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTOs.Request.Court
+{
+    public class CourtRequest
+    {
+        [Required(ErrorMessage = MessageConstant.Vi.Court.Require.CourtCodeRequired)]
+        public string CourtCode { get; set; }
+        [Required(ErrorMessage = MessageConstant.Vi.Court.Require.TypeOfCourtRequired)]
+        public int TypeOfCourtId { get; set; }
+        [Required(ErrorMessage = MessageConstant.Vi.Court.Require.BadmintonCourtRequired)]
+        public int BadmintonCourtId { get; set; }
+    }
+}
---
+++ DTOs/Response/Court/CourtDto.cs
@@ -0,0 +1,19 @@
+﻿using BusinessObjects;
+using BusinessObjects.Constants;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTOs.Response.Court
+{
+    public class CourtDto
+    {
+        public int Id { get; set; }
+        public string CourtCode { get; set; }
+        public int TypeOfCourtId { get; set; }
+        public int BadmintonCourtId { get; set; }
+    }
+}
